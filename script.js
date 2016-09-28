619f2ebe91c175dfe330950fbb06147cade5a68b
sophiayangx <sopyang@nuevaschool.org>
2016-09-28 08:44:55 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ var colorNameDisplay = $("#color-display")
 
 colorfulElements.hover(function(e) {
   var elementHoveredOver = $(e.target)
-  colorNameDisplay.html("hi");
+  colorNameDisplay.html("elementHoveredOver");
 })
